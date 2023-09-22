f9a2e29d3599fcf6d0faf8784523a60a67552478
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-09-22 13:18:28 +0200

    220923

--- CPR180923_00/JS/index.js
+++ CPR180923_00/JS/index.js
@@ -13,3 +13,51 @@ const changeBackgroundColor = () => {
 
 // Agrear un evento al botón para cambiar el color al hacer click.
 const changeColorBtn = document.getElementById("changeColorBtn");
+
+// Función para realizar operaciones matemáticas
+function calculate() {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  const operator = document.getElementById('operator').value;
+  let result;
+
+  switch (operator) {
+      case 'add':
+          result = num1 + num2;
+          break;
+      case 'subtract':
+          result = num1 - num2;
+          break;
+      case 'multiply':
+          result = num1 * num2;
+          break;
+      case 'divide':
+          if (num2 === 0) {
+              result = 'No se puede dividir por cero';
+          } else {
+              result = num1 / num2;
+          }
+          break;
+      default:
+          result = 'Operación no válida';
+  }
+
+  document.getElementById('result').textContent = 'Resultado: ' + result;
+}
+
+// Función para reiniciar el formulario
+function resetForm() {
+  document.getElementById('calculator-form').reset();
+  document.getElementById('result').textContent = '';
+}
+
+// Manejar el envío del formulario
+document.getElementById('calculator-form').addEventListener('submit', function (e) {
+  e.preventDefault(); // Evitar el envío del formulario
+  calculate();
+});
+
+// Manejar el botón de reinicio
+document.getElementById('reset').addEventListener('click', function () {
+  resetForm();
+});
